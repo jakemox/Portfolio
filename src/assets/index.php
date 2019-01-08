04de4ce40f3613f094fa0311b0bef5440316c243
Jake Moxon <jake_mox@hotmail.com>
2019-01-08 11:39:41 +0000

    added scratchmap

--- src/assets/index.php
+++ src/assets/index.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="style.css">
     <link href="https://fonts.googleapis.com/css?family=Poppins:200,400,500" rel="stylesheet">
-    <link rel="icon" href="Favicon-3.png" sizes="16x16 32x32" type="image/png">
+    <link rel="icon" href="images/favicon.png" sizes="32x32" type="image/png">
 </head>
 
 
@@ -42,127 +42,40 @@
             </div>
         </div>
     </header>
-    <section class="general">
-        <div class="intro">
-            <p>
-                Let's work together to make <b>your product stand out</b> . I create beautiful and functional websites. I work with my clients step by step <b>from the idea stage until the fully functioning web application</b>.
-            </p>
-        </div>
-    </section>
-    <section class="discover">
-        <div class="container">
-            <div class="dds">
-                <div class="ddd">
-                    <h3>Discover</h3>
-                    <p>Tell me about your project and we brainstorm together the features to create a plan for your product to launch.</p>
-                </div>
-                <div class="ddd">
-                    <h3>Design</h3>
-                    <p>We design the wireframe and agree together how to create the best experience for your users to present your brand.</p>
-                </div>
-                <div class="ddd">
-                    <h3>Deploy</h3>
-                    <p>We protoype and develop your product. We work together intensily to incorporate your feedback.</p>
-                </div>
-                <div class="ddd">
-                    <h3>Be Ready</h3>
-                    <p>We deploy your product and now your website is ready to get the traction. Get out there and grow your audience.</p>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="myskills">
-        <div class="container">
-            <h2 name="skills">My Skills</h2>
-            <div class="progress">
-                <div class="skill1">HTML</div>
-                <div class="bar">
-                    <div class="knob" style="width: 90%"></div>
-                </div>
-                <div class="perc">90%</div>
-            </div>
-            <div class="progress">
-                <div class="skill1">CSS</div>
-                <div class="bar">
-                    <div class="knob" style="width: 85%"></div>
-                </div>
-                <div class="perc">85%</div>
-            </div>
-            <div class="progress">
-                <div class="skill1">JavaScript</div>
-                <div class="bar">
-                    <div class="knob" style="width: 75%"></div>
-                </div>
-                <div class="perc">75%</div>
-            </div>
-            <div class="progress">
-                <div class="skill1">React</div>
-                <div class="bar">
-                    <div class="knob" style="width: 75%"></div>
-                </div>
-                <div class="perc">75%</div>
-            </div>
-            <div class="progress">
-                <div class="skill1">jQuery</div>
-                <div class="bar">
-                    <div class="knob" style="width: 80%"></div>
-                </div>
-                <div class="perc">80%</div>
-            </div>
-            <div class="progress">
-                <div class="skill1">PHP</div>
-                <div class="bar">
-                    <div class="knob" style="width: 20%"></div>
-                </div>
-                <div class="perc">20%</div>
-            </div>
-            <div class="progress">
-                <div class="skill1">Laravel</div>
-                <div class="bar">
-                    <div class="knob" style="width: 10%"></div>
-                </div>
-                <div class="perc">10%</div>
-            </div>
-            <div class="progress">
-                <div class="skill1">UX</div>
-                <div class="bar">
-                    <div class="knob" style="width: 30%"></div>
-                </div>
-                <div class="perc">30%</div>
-            </div>
-        </div>
-    </section>
+
     <section class="portfolio">
         <div class="container">
             <h2 name="folio">My Portfolio</h2>
             <div class="projects">
                 <div class="ppp">
-                    <div id="hoverarea" class="img photo1" style="background-image: url('/images/Bow-River.jpg')">
-                        <a class="project1" href="">
-                            <div id="hovereffect" class="place hover1"><span>Banff</span></div>
+                    <div class="img photo1">
+                        <a class="project1" href="http://scratchmyapp.com" target="blank">
+                            <div class="place hover1">
+                                <img src="images/scratch-logo.png" alt="">
+                            </div>
                         </a>
                     </div>
-                    <div class="img photo2" style="background-image: url('/images/Tallinn-3.jpg')">
+                    <div class="img photo2">
                         <a class="project1" href="">
                             <div class="place hover2"><span>Tallinn</span></div>
                         </a>
                     </div>
-                    <div class="img photo3" style="background-image: url('/images/Tatra-Snow.jpg')">
+                    <div class="img photo3">
                         <a class="project1" href="">
                             <div class="place hover3"><span>Zakopane</span></div>
                         </a>
                     </div>
-                    <div class="img photo4" style="background-image: url('/images/Pigeon.jpg')">
+                    <div class="img photo4">
                         <a class="project1" href="">
                             <div class="place hover4"><span>Rome</span></div>
                         </a>
                     </div>
-                    <div class="img photo5" style="background-image: url('/images/Maya-Bay.jpg')">
+                    <div class="img photo5">
                         <a class="project1" href="">
                             <div class="place hover5"><span>Thailand</span></div>
                         </a>
                     </div>
-                    <div class="img photo6" style="background-image: url('/images/Lake-Bled.jpg')">
+                    <div class="img photo6">
                         <a class="project1" href="">
                             <div class="place hover6"><span>Bled</span></div>
                         </a>
@@ -171,6 +84,67 @@
             </div>
         </div>
     </section>
+    
+    <section class="myskills">
+        <div class="container">
+            <h2 name="skills">My Skills</h2>
+            <div class="progress">
+                <div class="skill1">HTML</div>
+                <div class="bar">
+                    <div class="knob" style="width: 90%"></div>
+                </div>
+            </div>
+            <div class="progress">
+                <div class="skill1">CSS</div>
+                <div class="bar">
+                    <div class="knob" style="width: 90%"></div>
+                </div>
+            </div>
+            <div class="progress">
+                <div class="skill1">JavaScript</div>
+                <div class="bar">
+                    <div class="knob" style="width: 80%"></div>
+                </div>
+            </div>
+            <div class="progress">
+                <div class="skill1">React</div>
+                <div class="bar">
+                    <div class="knob" style="width: 70%"></div>
+                </div>
+            </div>
+            <div class="progress">
+                <div class="skill1">PHP</div>
+                <div class="bar">
+                    <div class="knob" style="width: 50%"></div>
+                </div>
+            </div>
+            <div class="progress">
+                <div class="skill1">Laravel</div>
+                <div class="bar">
+                    <div class="knob" style="width: 60%"></div>
+                </div>
+            </div>
+            <div class="progress">
+                <div class="skill1">UX</div>
+                <div class="bar">
+                    <div class="knob" style="width: 90%"></div>
+                </div>
+            </div>
+            <div class="progress">
+                <div class="skill1">Photoshop</div>
+                <div class="bar">
+                    <div class="knob" style="width: 100%"></div>
+                </div>
+            </div>
+            <div class="progress">
+                <div class="skill1">Illustrator</div>
+                <div class="bar">
+                    <div class="knob" style="width: 90%"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <section class="aboutme">
         <div class="container">
             <h2 name="info">About Me</h2>
@@ -178,21 +152,13 @@
                 <img class="me" src="images/Jake.jpg" alt="Jake Moxon">
                 <div class="text">
                     <p>
-                        <b>My name is Jake Moxon.</b> I am a full-stack web developer and enjoy learning new trends. <b>I am developing user interfaces in JavaScript and React and backend of my projects in PHP and Laravel.</b>
-                        <br>
-                        <br>
-                        I am open to learning new technologies and enjoy working with people who are as passionate about all web-related as me.
-                        <br>
-                        <br>
-                        I have worked for clients all across the world but currently I am based in Prague, Czech Republic. 
-                        <br>
-                        <br>
-                        I like cats, gaming, and snowboarding. If I do not develop, you can also meet me trekking in the mountains.
+                    A Junior Front-End Developer, I have recently enhanced my skills in web development by studying for 3 months in a Coding Bootcamp in Prague. Throughout my life, I have combined great problem solving and attention to detail with creative flair. Combining my coding skills with my keen eye for design, I have a very good base to build upon in front-end web development.  My reliability and willingness to learn would make me a valuable asset to any team and  I am hungry to fulfil my potential as a web developer. I am available to work immediately. 
                     </p>
                 </div>
             </div>
         </div>
     </section>
+
     <section class="findme">
         <div class="container">
             <h2>Find me here</h2>
@@ -203,6 +169,7 @@
             </div>
         </div>
     </section>
+
     <section class="contactme">
         <div class="container">
             <h2 name="contact">Contact Me</h2>
@@ -222,12 +189,14 @@
             <!-- </div> -->
         </div>
     </section>
+
     <section class="nice">
         <div class="container">
             <h2>Nice to meet you!</h2>
             <p>Copyright &copy; Jake Moxon</p>
         </div>
     </section>
+
     <script src="main.js"></script>
 </body>
 
