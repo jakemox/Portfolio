a2188c286b2e12910a43e70a2adf2bb71760ba07
Jake Moxon <jake_mox@hotmail.com>
2019-01-10 21:12:48 +0000

    changes suggested by Ashleigh

--- src/assets/index.php
+++ src/assets/index.php
@@ -20,7 +20,7 @@
     <header>
         <div class="container">
             <div class="main">
-                <div class="abc">
+                <div class="nav-container">
                     <a href="index.html">
                         <img id="logo" class="logo" src="images/Favicon 4.svg" alt="Logo">
                     </a>   
@@ -47,49 +47,47 @@
         <div class="container">
             <h2 name="folio">My Portfolio</h2>
             <div class="projects">
-                <div class="ppp">
-                    <div class="img photo1">
-                        <a class="project1" href="http://scratchmyapp.com" target="blank">
-                            <div class="place hover1">
-                                <span class="folio-title">ScratchMyApp</span>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="img photo2">
-                        <a class="project1" href="/weather-app" target="blank">
-                            <div class="place hover2">
-                                <span class="folio-title">Weather App</span>
-                                <span class="tech">(React)</span>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="img photo3">
-                        <a class="project1" href="/sky-scammer" target="blank">
-                            <div class="place hover3">
-                                <span class="folio-title">Flight App</span>
-                                <span class="tech">(React)</span>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="img photo4">
-                        <a class="project1" href="/exchange-rates" target="blank">
-                            <div class="place hover4">
-                                <span class="folio-title">Exchange Rates</span>
-                                <span class="tech">(JavaScript)</span>
-                            </div>
-                        </a>
-                    </div>
-                    <!-- <div class="img photo5">
-                        <a class="project1" href="">
-                            <div class="place hover5"><span>Thailand</span></div>
-                        </a>
-                    </div>
-                    <div class="img photo6">
-                        <a class="project1" href="">
-                            <div class="place hover6"><span>Bled</span></div>
-                        </a>
-                    </div> -->
+                <div class="img photo1">
+                    <a class="project1" href="http://scratchmyapp.com" target="blank">
+                        <div class="place hover1">
+                            <span class="folio-title">ScratchMyApp</span>
+                        </div>
+                    </a>
                 </div>
+                <div class="img photo2">
+                    <a class="project1" href="/weather-app" target="blank">
+                        <div class="place hover2">
+                            <span class="folio-title">Weather App</span>
+                            <span class="tech">(React)</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="img photo3">
+                    <a class="project1" href="/sky-scammer" target="blank">
+                        <div class="place hover3">
+                            <span class="folio-title">Flight App</span>
+                            <span class="tech">(React)</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="img photo4">
+                    <a class="project1" href="/exchange-rates" target="blank">
+                        <div class="place hover4">
+                            <span class="folio-title">Exchange Rates</span>
+                            <span class="tech">(JavaScript)</span>
+                        </div>
+                    </a>
+                </div>
+                <!-- <div class="img photo5">
+                    <a class="project1" href="">
+                        <div class="place hover5"><span>Thailand</span></div>
+                    </a>
+                </div>
+                <div class="img photo6">
+                    <a class="project1" href="">
+                        <div class="place hover6"><span>Bled</span></div>
+                    </a>
+                </div> -->
             </div>
         </div>
     </section>
@@ -100,55 +98,55 @@
             <div class="progress">
                 <div class="skill1">HTML</div>
                 <div class="bar">
-                    <div class="knob" style="width: 90%"></div>
+                    <div class="knob" style="width: 80%"></div>
                 </div>
             </div>
             <div class="progress">
                 <div class="skill1">CSS</div>
                 <div class="bar">
-                    <div class="knob" style="width: 90%"></div>
+                    <div class="knob" style="width: 80%"></div>
                 </div>
             </div>
             <div class="progress">
                 <div class="skill1">JavaScript</div>
                 <div class="bar">
-                    <div class="knob" style="width: 80%"></div>
+                    <div class="knob" style="width: 70%"></div>
                 </div>
             </div>
             <div class="progress">
                 <div class="skill1">React</div>
                 <div class="bar">
-                    <div class="knob" style="width: 70%"></div>
+                    <div class="knob" style="width: 60%"></div>
                 </div>
             </div>
             <div class="progress">
                 <div class="skill1">PHP</div>
                 <div class="bar">
-                    <div class="knob" style="width: 50%"></div>
+                    <div class="knob" style="width: 40%"></div>
                 </div>
             </div>
             <div class="progress">
                 <div class="skill1">Laravel</div>
                 <div class="bar">
-                    <div class="knob" style="width: 60%"></div>
+                    <div class="knob" style="width: 50%"></div>
                 </div>
             </div>
             <div class="progress">
                 <div class="skill1">UX</div>
                 <div class="bar">
-                    <div class="knob" style="width: 90%"></div>
+                    <div class="knob" style="width: 80%"></div>
                 </div>
             </div>
             <div class="progress">
                 <div class="skill1">Photoshop</div>
                 <div class="bar">
-                    <div class="knob" style="width: 100%"></div>
+                    <div class="knob" style="width: 90%"></div>
                 </div>
             </div>
             <div class="progress">
                 <div class="skill1">Illustrator</div>
                 <div class="bar">
-                    <div class="knob" style="width: 90%"></div>
+                    <div class="knob" style="width: 80%"></div>
                 </div>
             </div>
         </div>
@@ -157,11 +155,11 @@
     <section class="aboutme" id="info">
         <div class="container">
             <h2 name="info">About Me</h2>
-            <div class="aaa">
+            <div class="about-content">
                 <img class="me" src="images/Jake.jpg" alt="Jake Moxon">
                 <div class="text">
                     <p>
-                    A Junior Front-End Developer, I have recently enhanced my skills in web development by studying for 3 months in a Coding Bootcamp in Prague. Throughout my life, I have combined great problem solving and attention to detail with creative flair. Combining my coding skills with my keen eye for design, I have a very good base to build upon in front-end web development.  My reliability and willingness to learn would make me a valuable asset to any team and  I am hungry to fulfil my potential as a web developer. I am available to work immediately. 
+                    A keen and creative Junior Developer, I have recently enhanced my skills in web development by studying for 3 months in an intensive Coding Bootcamp. I am seeking a role in which I can work in a great team to further develop my Javascript and OOP skills while making a meaningful contribution. Combining my coding skills with my keen eye for design, I have a good base to build upon and am eager for a challenge. My reliability and willingness to learn make me a valuable asset to any team and I am hungry to fulfil my potential as a web developer.
                     </p>
                 </div>
             </div>
@@ -174,16 +172,16 @@
             <div class="social">
                 <a href="https://www.instagram.com/jakemox99/" target="_blank"><img class="icons" src="images/instagram.svg" alt="instagram"></a>
                 <a href="https://github.com/jakemox" target="_blank"><img class="icons" src="images/github.svg" alt="github"></a>
-                <a href="https://www.linkedin.com/in/jake-moxon-74a541134/" target="_blank"><img class="icons" src="images/linkedin.svg" alt="linkedIn"></a>
+                <a href="https://www.linkedin.com/in/jakemox/" target="_blank"><img class="icons" src="images/linkedin.svg" alt="linkedIn"></a>
             </div>
         </div>
     </section>
 
-    <section class="contactme" id="contactme">
+    <!-- <section class="contactme" id="contactme">
         <div class="container">
-            <h2 name="contact">Contact Me</h2>
+            <h2 name="contact">Contact Me</h2> -->
             <!-- <div class="form"> -->
-                <form class="form" id="contact" action="contact.php" method="post">
+                <!-- <form class="form" id="contact" action="contact.php" method="post">
                     <div class="field1">
                         <img src="images/profile-01.svg" alt="" width="40px" height="40px">
                         <input name="name" placeholder="Your Name*" type="text" tabindex="1" required>
@@ -193,11 +191,11 @@
                         <input name="email" placeholder="Your Email*" type="email" tabindex="3" required>
                     </div>
                     <div class="field2"><p>Your message*</p><textarea name="message" placeholder="Your message*" tabindex="5" required></textarea></div>
-                    <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Send Message</button>
-                </form>
+                    <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Send Message</button> -->
+                <!-- </form> -->
             <!-- </div> -->
-        </div>
-    </section>
+        <!-- </div>
+    </section> -->
 
     <section class="nice">
         <div class="container">
