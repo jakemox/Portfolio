b20ac66a2258fd0ad91c0acc491879eebd4bca83
Jake Moxon <jake_mox@hotmail.com>
2019-04-14 22:03:18 +0100

    added public

---
+++ build/precache-manifest.0db7d9ba66a119c863db931cc571398c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "21d6ea8d8f53c6af16a7bfccdea4b07b",
+    "url": "/static/media/scratchmap.21d6ea8d.jpg"
+  },
+  {
+    "revision": "9a1661c13e583052d206",
+    "url": "/static/css/main.7b776cab.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "53cfa18cf9abae46e79a",
+    "url": "/static/js/2.c839b8c7.chunk.js"
+  },
+  {
+    "revision": "aa82f4e3ebde66556015bb2d1746ef79",
+    "url": "/static/media/cross.aa82f4e3.svg"
+  },
+  {
+    "revision": "a66169d10cba46599d9f724f2b6593b0",
+    "url": "/static/media/montreal.a66169d1.jpg"
+  },
+  {
+    "revision": "cfdd61e84a77b56d5de20ff8a68e0d9c",
+    "url": "/static/media/logo.cfdd61e8.svg"
+  },
+  {
+    "revision": "bafc45f797efed064a7400c95171c05f",
+    "url": "/static/media/burger.bafc45f7.svg"
+  },
+  {
+    "revision": "78b7ab9be53780fabc6b92fbc69aa453",
+    "url": "/static/media/jake.78b7ab9b.jpg"
+  },
+  {
+    "revision": "9a1661c13e583052d206",
+    "url": "/static/js/main.e572f36e.chunk.js"
+  },
+  {
+    "revision": "6607915d7d2dc42064ecf416d340c017",
+    "url": "/static/media/luxus.6607915d.jpg"
+  },
+  {
+    "revision": "8bc1229e8a21c0b3efb07a78c2d0ae72",
+    "url": "/static/media/skyscammer.8bc1229e.jpg"
+  },
+  {
+    "revision": "d4ffdc7367bb6e11e78448b42b51ac43",
+    "url": "/static/media/weather.d4ffdc73.jpg"
+  },
+  {
+    "revision": "f2550379f455d3b221e2dc1dd6a2e3e7",
+    "url": "/static/media/exchange.f2550379.jpg"
+  },
+  {
+    "revision": "ce5121da6400ea984adb3485ac55b849",
+    "url": "/static/media/linkedin.ce5121da.svg"
+  },
+  {
+    "revision": "c5c8af23ab414ea3bf4fd8268fdd67dd",
+    "url": "/static/media/github.c5c8af23.svg"
+  },
+  {
+    "revision": "efb334253f9db73b082fdb8c0dac63b5",
+    "url": "/static/media/instagram.efb33425.svg"
+  },
+  {
+    "revision": "faddf783bc357a68bd0fb3c98d3cbd51",
+    "url": "/index.html"
+  }
+];
